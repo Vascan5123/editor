31f30f19d0196462156be69ad27028bea38ae5b6
Lupan Vasile <66137225+Vascan5123@users.noreply.github.com>
2022-03-02 11:34:56 +0200

    add pdf_files_views

--- extend.php
+++ extend.php
@@ -12,13 +12,21 @@
 namespace Vascan\Editor;
 
 use Flarum\Extend;
+use s9e\TextFormatter\Configurator;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/resources/less/forum.less'),
+        ->js(__DIR__ . '/js/dist/forum.js')
+        ->css(__DIR__ . '/resources/less/forum.less'),
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
-        ->css(__DIR__.'/resources/less/admin.less'),
-    new Extend\Locales(__DIR__ . '/resources/locale')
+        ->js(__DIR__ . '/js/dist/admin.js')
+        ->css(__DIR__ . '/resources/less/admin.less'),
+    new Extend\Locales(__DIR__ . '/resources/locale'),
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+            $config->BBCodes->addCustom(
+                '[pdf]{TEXT}[/pdf]',
+                '<iframe src="" style="width:100%; height:500px;" class="iframe_pdf" frameborder="0">{TEXT}</iframe><span class="span_pdf">{TEXT}</span>'
+            );
+        })
 ];
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,5 +1,7 @@
 import Editor from './components/Editor.js';
+import Pdf_Files from './components/pdf_files.js';
 
 app.initializers.add('vascan/editor', () => {
-  Editor()
+  Editor(),
+  Pdf_Files()
 });
